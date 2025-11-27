59b2e59d9a0a959e8c77710da5eecb73e870a018
Alejandro Meneses <garciamenesesale@gmail.com>
2025-11-27 12:23:27 -0500

    feat se creo el menu principal de la calculadora dentro de un ciclo while para la ejecución del programa

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ let proyectosIngresado = false;
 let parcialIngresado = false;
 let finalIngresado = false;
 let continuar = true;
+
+while (continuar) {
+    let opcion = prompt(
+        "                        Calculadora de Notas - Academia Full-Stack          \n"+
+    "                                             =Menú Principal=\n" + 
+    "                                1. Ingresar Nota de Proyectos\n"+
+    "                                2. Ingresar Nota de Examen Parcial\n"+
+    "                                3. Ingresar Nota de Examen Final\n"+
+    "                                4. Calcular Definitiva y Estado\n"+
+    "                                5. Salir\n"+
+    "                                \nSeleccione una opción:"
+  );
+}
