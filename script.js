12c4d36739506dba426df871d85f5a59503011f3
Mafer_Hernandez <hernandezmf05@hotmail.com>
2025-11-28 13:03:39 -0500

    Feat se agrega funcionalidad de validacion de la opcion ingresada por el usuario

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
+// 1. DECLARACIÓN DE VARIABLES
+
+// Contadores de votos para cada opción
 let contadorVotosA = 0;
 let contadorVotosB = 0;
 let contadorVotosBlanco = 0;
 
+// Variables de control del menú y validación
 let opcionSeleccionada = 0;
 let codigoIngresado = 0;
 
+//2. CICLO PRINCIPAL DEL SISTEMA
+// El sistema se ejecuta mientras el usuario no elija salir (opción 5)
 
 while (opcionSeleccionada !== 5) {
 
   opcionSeleccionada = Number(prompt(
-    "Menú del sistema de votación\n" +
-    "1. Elegir candidato A\n" +
-    "2. Elegir candidato B\n" +
-    "3. Voto en blanco\n" +
-    "4. Ver resultados\n" +
+    "Sistema de Votación DemoVoto\n" +
+    "--------------------------------\n" +
+    "1. Votar por Candidato A\n" +
+    "2. Votar por Candidato B\n" +
+    "3. Votar en Blanco\n" +
+    "4. Consultar Resultados Parciales\n" +
     "5. Salir"
   ));
 
-
 }
-
