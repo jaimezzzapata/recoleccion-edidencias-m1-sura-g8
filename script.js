c240fd9a7b3ac6f35402849ce417bd92aa256727
Mafer_Hernandez <hernandezmf05@hotmail.com>
2025-11-28 13:01:12 -0500

    Feat se declaran variables y se realiza menu de opciones

---
+++ script.js
@@ -0,0 +1,22 @@
+let contadorVotosA = 0;
+let contadorVotosB = 0;
+let contadorVotosBlanco = 0;
+
+let opcionSeleccionada = 0;
+let codigoIngresado = 0;
+
+
+while (opcionSeleccionada !== 5) {
+
+  opcionSeleccionada = Number(prompt(
+    "Menú del sistema de votación\n" +
+    "1. Elegir candidato A\n" +
+    "2. Elegir candidato B\n" +
+    "3. Voto en blanco\n" +
+    "4. Ver resultados\n" +
+    "5. Salir"
+  ));
+
+
+}
+
