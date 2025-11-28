de3038d1e4edf51933f3b0aed4b73784c4911c0b
Jeisson Yela <jeissoyela@gmail.com>
2025-11-28 02:34:37 -0500

    Agregar pantalla de bienvenida

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ let pesoUsuario;
 let alturaUsuario;
 let indiceMasaCorporal;
 let sistemaActivo = true;
+
+console.log("=== Bienvenido a VidaSana - Asistente de Salud ===");
+console.log("Sistema de cálculo de Índice de Masa Corporal (BMI)\n");
