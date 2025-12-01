4215b25c10cb0d0d4d62b6a92c5966d01dfa6117
jeanc <chinchajoa03@gmail.com>
2025-12-01 01:37:37 -0500

    feat se crean variables y se establece el menu principal con ciclo while

---
+++ script.js
@@ -0,0 +1,16 @@
+/*==========SIMULADOR DE CHECKOUT "TIENDA WEB"=================*/
+let subTotal= 0
+let descuentoAplicado= 0
+let cuponUsado= false
+let costoEnvio= 0
+let menu= 0
+
+while(menu){
+    console.log("\n===== 🛒 MENÚ PRINCIPAL =====")
+    console.log("1) Agregar Producto (Precio fijo: 50.000)")
+    console.log("2) Aplicar Cupón de Descuento")
+    console.log("3) Calcular Costo de Envío")
+    console.log("4) Calcular Total a Pagar")
+    console.log("5) Salir")
+
+}
